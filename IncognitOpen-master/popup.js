63e75d3db2bb7c2079b9ed196d6a69176fec6fe7
BorisHu <91997134+BorisHu@users.noreply.github.com>
2023-01-22 23:44:55 -0800

    Modified and added browser history delete

--- IncognitOpen-master/popup.js
+++
@@ -1,8 +0,0 @@
-let openLinkBtn = document.getElementById('openLink');
-
-openLinkBtn.onclick = function (element) {
-    chrome.windows.getCurrent({ populate: true}, function(window){
-        const currentTab = window.tabs.find(tab => tab.active)
-        chrome.windows.create({ url: currentTab.url, incognito: true });
-    })
-};
--- background.js
+++ background.js
@@ -1,16 +1,9 @@
-function clickAction(info) {
-    const url = info.linkUrl ?? info.pageUrl;
-  
-    chrome.windows.getAll({ windowTypes: ["normal"] }).then((windows) => {
-      const incognitoWin = windows.find((w) => w.incognito === true);
-      const incognitoId = incognitoWin?.id ?? null;
-  
-      if (incognitoId !== null) {
-        chrome.tabs.create({ windowId: incognitoId, url: url }).then((tab) => {
-          chrome.tabs.update(tab.id, { active: true });
-        });
-      } else {
-        chrome.windows.create({ url: url, incognito: true });
-      }
-    });
-  }
+chrome.browserAction.onClicked.addListener(function ({ url }) {
+  chrome.windows.create({
+    url,
+    incognito: true,
+  });
+  chrome.history.deleteUrl({
+    url
+  })
+});
